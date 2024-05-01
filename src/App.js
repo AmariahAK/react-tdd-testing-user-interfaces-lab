b4fa6a110aefe60902842fab24c66ddc8f0fabab
Amariah <amariah.abish@gmail.com>
2024-05-01 18:17:28 +0300

    full test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+
+import React from "react";
+
 function App() {
-  return <div>Your code here</div>;
+  return (
+    <div>
+      <h1>Hi, I'm (your name)</h1>
+      <img src="https://media.licdn.com/dms/image/D4D35AQHyUoyDahd99Q/profile-framedphoto-shrink_400_400/0/1704949500964?e=1715184000&v=beta&t=fnbKxxlO_t9oyKrUt4lnlRm-Lo1QCdhxs-OrvbNrolY" alt="Amariah Kamau" />
+      <h2>About Me</h2>
+      <p>Biography here</p>
+      <a href="https://github.com/AmariahAK">GitHub</a>
+      <a href="https://www.linkedin.com/in/amariah-kamau-3156412a6/">LinkedIn</a>
+    </div>
+  );
 }
 
 export default App;
